e9d6167bed4225ad7039e7149977f09d85834661
yunxi <mc90716@163.com>
2016-10-27 11:09:54 +0800

    add IndexTimeType

---
+++ src/com/enumeration/IndexTimeType.java
@@ -0,0 +1,31 @@
+package com.enumeration;
+
+public enum IndexTimeType {
+	YEAR {
+		@Override
+		public String getIndexTime(String time) {
+			return time.substring(0, 4);
+		}
+	},
+	MONTH {
+		@Override
+		public String getIndexTime(String time) {
+			return time.substring(0, 4) + time.substring(5, 7);
+		}
+	},
+	DAY {
+		@Override
+		public String getIndexTime(String time) {
+			return time.substring(0, 4) + time.substring(5, 7) + time.substring(8, 10);
+		}
+	};
+	
+	public abstract String getIndexTime(String time);
+	
+	public static void main(String[] args) {
+		IndexTimeType year = IndexTimeType.valueOf("YEAR");
+		//IndexTimeType month = IndexTimeType.valueOf("MONTH");
+		//IndexTimeType day = IndexTimeType.valueOf("DAY");
+		System.out.println(year.getIndexTime("2016-10-26T09:18:21.779Z"));
+	}
+}
